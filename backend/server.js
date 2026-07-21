320e760ef5b81e545492c3a1a6a343467b73bff2
Pranav <pranavsharma@Pranavs-MacBook-Air.local>
2026-07-21 15:05:56 +0530

    Prepare project for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res)=>{
     res.send("Backend is running")
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`)
